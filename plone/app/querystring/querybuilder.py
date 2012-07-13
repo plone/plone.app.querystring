0e039913c2e9920a616f13c45a9c3db4fc8d7cc4
Timo Stollenwerk <contact@timostollenwerk.net>
2012-07-13 11:04:42 +0200

    Tests for querybuildout._makeQuery method added. Make sure the Subject query field is only encoded if the query is a unicode string.

--- plone/app/querystring/querybuilder.py
+++ plone/app/querystring/querybuilder.py
@@ -72,9 +72,16 @@ class QueryBuilder(BrowserView):
             parsedquery['path'] = getNavigationRoot(self.context)
 
         # The Subject field in Plone currently uses a utf-8 encoded string.
+        # When a catalog query tries to compare a unicode string from the
+        # parsedquery with existing utf-8 encoded string indexes unindexing
+        # will fail with a UnicodeDecodeError. To prevent this from happening
+        # we always encode the Subject query.
+        # XXX: As soon as Plone uses unicode for all indexes, this code can
+        # be removed.
         if 'Subject' in parsedquery:
-            parsedquery['Subject']['query'] = [
-                x.encode("utf-8") for x in parsedquery['Subject']['query']]
+            if isinstance(parsedquery['Subject']['query'], unicode):
+                parsedquery['Subject']['query'] = \
+                    parsedquery['Subject']['query'].encode("utf-8")
 
         results = catalog(parsedquery)
         if not brains:
