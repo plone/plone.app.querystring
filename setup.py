2f800d10c2b380e8c363b4b3ffe11a4162f5767a
Timo Stollenwerk <tisto@plone.org>
2015-07-18 08:24:52 +0200

    Back to development: 1.3.4

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.3.3'
+version = '1.3.4.dev0'
 
 long_description = open("README.rst").read() + "\n"
 long_description += open("CHANGES.rst").read()
