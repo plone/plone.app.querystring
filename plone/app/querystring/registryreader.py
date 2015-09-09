9496f0cfda20b6ad3c3f43d5744b6e8fcc8a79c9
Winston Ferreira <winstonf88@gmail.com>
2015-09-09 17:16:00 -0300

    Fixed QuerystringRegistryReader.mapSortableIndex so it doesnt return ZCTextIndexes

--- plone/app/querystring/registryreader.py
+++ plone/app/querystring/registryreader.py
@@ -6,6 +6,9 @@ from zope.i18n import translate
 from zope.i18nmessageid import Message
 from zope.interface import implements
 from zope.schema.interfaces import IVocabularyFactory
+from Products.ZCTextIndex.interfaces import IZCTextIndex
+from Products.CMFCore.utils import getToolByName
+
 import logging
 
 logger = logging.getLogger("plone.app.querystring")
@@ -102,9 +105,11 @@ class QuerystringRegistryReader(object):
 
     def mapSortableIndexes(self, values):
         """Map sortable indexes"""
+        catalog = getToolByName(self.context, 'portal_catalog')._catalog
         sortables = {}
         for key, field in values.get('%s.field' % self.prefix).iteritems():
-            if field['sortable']:
+            if field['sortable'] and \
+               not IZCTextIndex.providedBy(catalog.getIndex(key)):
                 sortables[key] = values.get('%s.field.%s' % (self.prefix, key))
         values['sortable'] = sortables
         return values
