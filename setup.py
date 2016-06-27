e544fd23ab60d147f8704ed0ffd462c072e6b66d
Maurits van Rees <maurits@vanrees.org>
2016-06-27 13:48:53 +0200

    Back to development: 1.2.11
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.2.10'
+version = '1.2.11.dev0'
 
 long_description = open("README.rst").read() + "\n"
 long_description += open("CHANGES.rst").read()
