462fd05c6f4177ab34adc85133b629d7a05aa8f7
Maurits van Rees <maurits@vanrees.org>
2016-08-18 02:53:57 +0200

    Preparing release 1.4
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.4.0.dev0'
+version = '1.4'
 
 long_description = open("README.rst").read() + "\n"
 long_description += open("CHANGES.rst").read()
