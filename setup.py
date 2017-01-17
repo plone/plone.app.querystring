338b46eea4fbb6586bf1df02494997cee18c2bc4
Maurits van Rees <maurits@vanrees.org>
2017-01-17 19:21:48 +0100

    Back to development: 1.4.3
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-version = '1.4.2'
+version = '1.4.3.dev0'
 
 long_description = open("README.rst").read() + "\n"
 long_description += open("CHANGES.rst").read()
