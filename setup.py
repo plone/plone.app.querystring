3b3c09b3c6f303d278a7a7a07b1b5cfc346830eb
esteele <eric@esteele.net>
2017-02-05 14:30:11 -0500

    Preparing release 1.3.17
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-version = '1.3.17.dev0'
+version = '1.3.17'
 
 long_description = open("README.rst").read() + "\n"
 long_description += open("CHANGES.rst").read()
