1177818831e4f6978ec3faa8c59539498f152eb7
esteele <eric@esteele.net>
2018-09-28 21:48:36 -0400

    Preparing release 1.4.9
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-version = '1.4.9.dev0'
+version = '1.4.9'
 
 long_description = open("README.rst").read() + "\n"
 long_description += open("CHANGES.rst").read()
