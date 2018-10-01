ded208f7103f696500d03165a1e0a9225de4cd67
Maurits van Rees <maurits@vanrees.org>
2018-10-01 15:35:18 +0200

    Fixed PyPI links in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     keywords='collection queries',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='https://pypi.python.org/pypi/plone.app.querystring',
+    url='https://pypi.org/project/plone.app.querystring',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone', 'plone.app'],
