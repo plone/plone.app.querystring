3d0fb9646e4b78398dcd52c385699ab2a89c5c58
esteele <eric@esteele.net>
2018-12-10 21:45:06 -0500

    Back to development: 1.4.12
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-version = '1.4.11'
+version = '1.4.12.dev0'
 
 long_description = open("README.rst").read() + "\n"
 long_description += open("CHANGES.rst").read()
