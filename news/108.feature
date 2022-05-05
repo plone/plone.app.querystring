68637cbc2e2d6c6c839875697131fdeba5480e9f
jackahl <kahl@kitconcept.com>
2022-05-05 18:34:32 +0200

    enable support to use vocabulary token in querystring-search

---
+++ news/108.feature
@@ -0,0 +1,1 @@
+add lookup to process vocabulary tokens being used as query parameters. Using values is also still supported. [jackahl]
--- plone/app/querystring/querymodifiers.py
+++ plone/app/querystring/querymodifiers.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from plone.app.querystring.interfaces import IQueryModifier
 from zope.interface import provider
+from zope.component import getUtility
+from plone.registry.interfaces import IRegistry
+from zope.schema.interfaces import IVocabularyFactory
 
 
 @provider(IQueryModifier)
@@ -24,4 +27,34 @@ def modify_query_to_enforce_navigation_root(query):
             'o': 'plone.app.querystring.operation.string.path',
             'v': '/',
         })
+
     return query
+
+@provider(IQueryModifier)
+def vocabulary_value_look_up(query):
+    registry = getUtility(IRegistry)
+
+    for query_param in query:
+        query_field_name = query_param.get('i')
+        query_vocabulary_name = registry.get(f"plone.app.querystring.field.{query_field_name}.vocabulary")
+        if query_vocabulary_name:
+            query_terms = query_param.get('v')
+            factory = getUtility(IVocabularyFactory, query_vocabulary_name)
+            vocabulary = factory(None)
+            new_terms = []
+            for query_term in query_terms:
+                try:
+                    vocabulary_term = vocabulary.getTerm(query_term)
+                except:
+                    for term in vocabulary:
+                        if term.token == query_term:
+                            new_terms.append(term.value)
+                            break
+        query_param['v']= new_terms
+        query_vocabulary_name = None
+
+    return query
+
+
+
+
