c2330484c5df8ae8f79478289e16e7d497d687ce
Maurits van Rees <maurits@vanrees.org>
2025-11-19 20:58:25 +0100

    Preparing release 3.0.0a1
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-version = "3.0.0a1.dev0"
+version = "3.0.0a1"
 
 long_description = open("README.rst").read() + "\n"
 long_description += open("CHANGES.rst").read()
