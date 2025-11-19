0103923a5be665a5511bb4f8f3d8c7be545913d3
Maurits van Rees <maurits@vanrees.org>
2025-11-19 20:58:34 +0100

    Back to development: 3.0.0a2
    [ci skip]

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 
-version = "3.0.0a1"
+version = "3.0.0a2.dev0"
 
 long_description = open("README.rst").read() + "\n"
 long_description += open("CHANGES.rst").read()
