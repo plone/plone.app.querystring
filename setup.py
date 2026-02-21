fc1d7cffd7aff2e3ae96f1fc74b73ff316eed1b7
wesleybl <wesleybl@gmail.com>
2026-02-21 13:23:50 -0300

    Fix python_requires

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     namespace_packages=["plone", "plone.app"],
     include_package_data=True,
     zip_safe=False,
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     install_requires=[
         "setuptools",
         "plone.app.contentlisting",
