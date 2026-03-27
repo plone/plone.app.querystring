59b66d09b4a598d84cec73b0ca845e84d0ddbb45
Jens W. Klein <jk@kleinundpartner.at>
2026-03-27 12:54:08 +0100

    Remove Subject index modifier (Python 2 leftover)
    The Subject IParsedQueryIndexModifier was a no-op: it returned the
    value unchanged since the utf-8 encoding branch was unreachable dead
    code after the early return added during the Python 3 migration.
    
    Remove the class, its ZCML registration, and the related tests.
    
    Fixes #155

--- src/plone/app/querystring/indexmodifiers/query_index_modifiers.py
+++ src/plone/app/querystring/indexmodifiers/query_index_modifiers.py
@@ -4,51 +4,6 @@ from zope.interface import implementer
 
 
 @implementer(IParsedQueryIndexModifier)
-class Subject:
-    """
-    The Subject field in Plone currently uses a utf-8 encoded string.
-    When a catalog query tries to compare a unicode string from the
-    parsedquery with existing utf-8 encoded string indexes unindexing
-    will fail with a UnicodeDecodeError. To prevent this from happening
-    we always encode the Subject query.
-
-    XXX: As soon as Plone uses unicode for all indexes, this code can
-    be removed.
-    """
-
-    def __call__(self, value):
-        return ("Subject", value)
-
-        # Get the query operator
-        op = None
-        if "query" in value:
-            op = "query"
-        elif "not" in value:
-            op = "not"
-
-        query = value[op]
-        # query can be a unicode string or a list of unicode strings.
-        if isinstance(query, str):
-            query = query.encode("utf-8")
-        elif isinstance(query, list):
-            # We do not want to change the collections' own query string,
-            # therefore we create a new copy of the list.
-            copy_of_query = list(query)
-            # Iterate over all query items and encode them if they are
-            # unicode strings
-            i = 0
-            for item in copy_of_query:
-                if isinstance(item, str):
-                    copy_of_query[i] = item.encode("utf-8")
-                i += 1
-            query = copy_of_query
-        else:
-            pass
-        value[op] = query
-        return ("Subject", value)
-
-
-@implementer(IParsedQueryIndexModifier)
 class base:
     """DateIndex query modifier
     see Products.PluginIndexes.DateIndex.DateIndex.DateIndex._convert function
